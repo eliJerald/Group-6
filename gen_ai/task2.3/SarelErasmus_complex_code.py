006d010c7f1b04bc110072ff2112af33ef62e1cc
Tweetiebird987 <sarelerasmus856@gmail.com>
2025-02-28 23:04:17 -0800

    Add ChatGPT suggestions for optimization

--- gen_ai/task2.3/SarelErasmus_complex_code.py
+++ gen_ai/task2.3/SarelErasmus_complex_code.py
@@ -2,62 +2,70 @@ from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Load the image
 file = r".\\Images\AEB.jpg"
-
 img = Image.open(file)
 
-r_values,g_values,b_values = img.split()
+# Convert image to NumPy array
+img_array = np.array(img)
 
-red_img = Image.merge('RGB', (r_values, g_values.point(lambda _: 0),b_values.point(lambda _: 0)))
-green_img = Image.merge('RGB', (r_values.point(lambda _: 0), g_values,b_values.point(lambda _: 0)))
-blue_img = Image.merge('RGB', (r_values.point(lambda _: 0), g_values.point(lambda _: 0),b_values))
+# Extract RGB channels
+r_values, g_values, b_values = img_array[:, :, 0], img_array[:, :, 1], img_array[:, :, 2]
 
-mean_img = []
-weight_img = []
+# Create grayscale images
+mean_img = np.mean(img_array, axis=2).astype(np.uint8)
+weight_img = (0.299 * r_values + 0.587 * g_values + 0.114 * b_values).astype(np.uint8)
 
-for r,g,b in zip(r_values.getdata(),g_values.getdata(),b_values.getdata()):
-   mean_img.append(int((r + g + b)/3))
-   weight_img.append(int(0.299 * r + 0.587 * g + 0.114 * b))
+# Create RGB channel images
+red_img = np.zeros_like(img_array)
+green_img = np.zeros_like(img_array)
+blue_img = np.zeros_like(img_array)
 
+red_img[:, :, 0] = r_values
+green_img[:, :, 1] = g_values
+blue_img[:, :, 2] = b_values
+
+# Convert back to Image objects
+red_img_pil = Image.fromarray(red_img)
+green_img_pil = Image.fromarray(green_img)
+blue_img_pil = Image.fromarray(blue_img)
+mean_img_pil = Image.fromarray(mean_img)
+weight_img_pil = Image.fromarray(weight_img)
+r_values_pil = Image.fromarray(r_values)
+
+# Plot images
 fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(10, 10))
 
+# Original image
 axes[0][0].imshow(img)
-title = f'Original Image\nResolution: {img.width}x{img.height}'
-axes[0][0].set_title(title)
+axes[0][0].set_title(f'Original Image\nResolution: {img.width}x{img.height}')
 
-axes[0][1].imshow(red_img)
-title = f'Red Channel\nResolution: {red_img.width}x{red_img.height}'
-axes[0][1].set_title(title)
+# RGB channel images
+axes[0][1].imshow(red_img_pil)
+axes[0][1].set_title(f'Red Channel\nResolution: {img.width}x{img.height}')
 
-axes[0][2].imshow(green_img)
-title = f'Green Channel\nResolution: {green_img.width}x{green_img.height}'
-axes[0][2].set_title(title)
+axes[0][2].imshow(green_img_pil)
+axes[0][2].set_title(f'Green Channel\nResolution: {img.width}x{img.height}')
 
-axes[0][3].imshow(blue_img)
-title = f'Blue Channel\nResolution: {blue_img.width}x{blue_img.height}'
-axes[0][3].set_title(title)
+axes[0][3].imshow(blue_img_pil)
+axes[0][3].set_title(f'Blue Channel\nResolution: {img.width}x{img.height}')
 
-mean_img = np.array(mean_img).reshape(-1,img.width)
+# Grayscale images
+axes[1][0].imshow(mean_img_pil, cmap="gray")
+axes[1][0].set_title(f'Grayscale (Average)\nResolution: {img.width}x{img.height}')
 
-axes[1][0].imshow(Image.fromarray(mean_img),cmap="gray")
-title = f'Grayscale (Average)\nResolution: {img.width}x{img.height}'
-axes[1][0].set_title(title)
+axes[1][1].imshow(weight_img_pil, cmap="gray")
+axes[1][1].set_title(f'Grayscale (Weight)\nResolution: {img.width}x{img.height}')
 
-weight_img = np.array(weight_img).reshape(-1,img.width)
+axes[1][2].imshow(r_values_pil, cmap="gray")
+axes[1][2].set_title(f'Grayscale (Red Channel)\nResolution: {img.width}x{img.height}')
 
-axes[1][1].imshow(Image.fromarray(weight_img),cmap="gray")
-title = f'Grayscale (Weight)\nResolution: {img.width}x{img.height}'
-axes[1][1].set_title(title)
+# Hide the last subplot
+axes[1][3].axis('off')
 
-r_values = np.array(r_values).reshape(-1,img.width)
-
-axes[1][2].imshow(Image.fromarray(r_values),cmap="gray")
-title = f'Grayscale (Red Channel)\nResolution: {img.width}x{img.height}'
-axes[1][2].set_title(title)
-
-# Hide the axis
+# Hide axes
 for ax in axes.flatten():
-   ax.axis('off')
+    ax.axis('off')
 
-plt.subplots_adjust(hspace=0,top=0.5)
+plt.subplots_adjust(hspace=0, top=0.5)
 plt.show()
